8a6dc425c495cb0c93d6b3420b06eec367a2206b
snkv0708 <sakarlov@edu.hse.ru>
2025-12-18 00:24:11 +0300

    async db

--- base.py
+++ base.py
@@ -1,22 +1,23 @@
 from datetime import datetime, timezone, timedelta
 
-from sqlalchemy import Column, Integer, String, Boolean, create_engine, DateTime
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import Column, Integer, String, Boolean, create_engine, DateTime, select
 from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.ext.mutable import MutableDict, MutableList
+from sqlalchemy.ext.mutable import MutableList
 from sqlalchemy.types import JSON
 
-engine = create_engine("sqlite:///users.db", echo=False)
+engine = create_async_engine("sqlite+aiosqlite:///users.db", echo=False)
 
 Base = declarative_base()
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer)
-    premium_until = Column(DateTime(timezone=True))
+    telegram_id = Column(Integer, unique=True)
+    premium_until = Column(DateTime)
     cur_model = Column(String)
     role = Column(String, nullable=True)
-    request_cnt = Column(Integer)
+    request_cnt = Column(Integer, nullable=False, default=0)
     context = Column(MutableList.as_mutable(JSON), default=list, nullable=False)
 
 
@@ -24,6 +25,9 @@ class User(Base):
         self.telegram_id = telegram_id
         self.premium_until = datetime(1970, 1, 1, tzinfo=timezone.utc)
         self.cur_model = "gpt-4o-mini"
+        self.role = None
+        self.request_cnt = 0
+        self.context = []
 
     # def __repr__(self):
     #     return "<User('%s', '%s', '%s')>" % (self.telegram_id, self.grade, self.messages_recieving)
@@ -31,97 +35,93 @@ class User(Base):
 
 # Создание таблицы
 
-SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+SessionLocal = async_sessionmaker(engine, expire_on_commit=False)
 
-def get_user(session, telegram_id: int) -> User | None:
-    return session.query(User).filter(User.telegram_id == telegram_id).first()
+async def get_user(session, telegram_id: int) -> User | None:
+    query = await session.execute(select(User).where(User.telegram_id == telegram_id))
+    return query.scalar_one_or_none()
 
-
-def add_user(telegram_id):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+async def add_user(telegram_id):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
                 return False
             else:
                 session.add(User(telegram_id=telegram_id))
                 return True
-    finally:
-        session.close()
 
-def update_premium(telegram_id):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+
+async def update_premium(telegram_id):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
-                user.premium_until = datetime.now(timezone.utc) + timedelta(days=30)
+                cur_time = datetime.utcnow()
+                if user.premium_until < cur_time:
+                    user.premium_until = cur_time + timedelta(days=30)
+                else:
+                    user.premium_until += timedelta(days=30)
                 return True
             else:
                 return False
-    finally:
-        session.close()
 
-def update_role(telegram_id, role):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+
+async def update_role(telegram_id, role):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
                 user.role = role
                 return True
             else:
                 return False
-    finally:
-        session.close()
 
 
-def update_model(telegram_id, model):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+
+async def update_model(telegram_id, model):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
-                user.model = model
+                user.cur_model = model
                 return True
             else:
                 return False
-    finally:
-        session.close()
 
-def update_request_cnt(telegram_id):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+
+async def update_request_cnt(telegram_id):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
                 user.request_cnt += 1
                 return True
             else:
                 return False
-    finally:
-        session.close()
 
-def update_context(telegram_id, role, text):
-    session = SessionLocal()
-    try:
-        with session.begin():
-            user = get_user(session, telegram_id)
+
+async def update_context(telegram_id, role, text):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
             if user:
                 user.context.append({"role": role, "content": text})
                 return True
             else:
                 return False
-    finally:
-        session.close()
-#
-# def delete_account(telegram_id):
-#     Session = sessionmaker(bind=engine)
-#     session = Session()
-#     session.query(User).filter_by(telegram_id=telegram_id).delete()
-#     session.commit()
-#     return
+
+
+async def delete_context(telegram_id):
+    async with SessionLocal() as session:
+        async with session.begin():
+            user = await get_user(session, telegram_id)
+            if user:
+                user.context.clear()
+                return True
+            else:
+                return False
+
 #
 # def get_all_tg_ids():
 #     Session = sessionmaker(bind=engine)
@@ -130,4 +130,8 @@ def update_context(telegram_id, role, text):
 #     session.commit()
 #     return data
 
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
+
+# async def init_db():
+#     async with engine.begin() as conn:
+#         await conn.run_sync(Base.metadata.create_all)
